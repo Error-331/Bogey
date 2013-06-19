a0a41f6c4b2f5304a61d232a404820512bc18286
root <sergei-System-Product-Name>
2013-06-19 11:46:46 +0300

    add deferred module

---
+++ modules/async/deferred.js
@@ -0,0 +1,25 @@
+var deferred = function()
+{
+    /* Private members starts here */
+    
+    var obj = this;
+    
+    // reject, fail, done, unknown
+    var status = 'unknown';
+    
+    /* Private members ends here */
+    
+    /* Privileged core methods starts here */
+    
+    this.done = function() {
+        
+    };
+    
+    this.when = function() {
+        
+    };
+    
+    /* Privileged core methods ends here */
+}
+
+exports.deferred = deferred;
