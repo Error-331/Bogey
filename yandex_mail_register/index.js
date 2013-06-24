a6eba6f626c61b905312ef397784a213d677c0aa
Error-331 <red331@mail.ru>
2013-06-25 00:21:36 +0300

    minor changes

--- yandex_mail_register/index.js
+++ yandex_mail_register/index.js
@@ -1,11 +1,8 @@
-var response = require('../modules/io/response');
-var sandboxutils = require('../modules/utils/sandboxutils');
-var captchabot = require('../modules/captchabot/captchabot');
-var antigate = require('../modules/antigate/antigate');
-var page = require('webpage');
-var curPage = page.create();
-var curPageURL = '';
+//var antigate = require('../modules/antigate/antigate');
+var yandexMail = require('../modules/yandex/mail');
 
+curYandexMail = yandexMail.create('');
+curYandexMail.openRegPage();
 
 //curCaptBot = captchabot.create('8f7fc1c96990c14cc3ebdbf752405140');
 //var def = curCaptBot.checkBalance();
@@ -14,25 +11,13 @@ var curPageURL = '';
 
 //phantom.cookiesEnabled = true;
 
-curAntigateBot = antigate.create('');
-var def = curAntigateBot.checkBalance();
+//curAntigateBot = antigate.create('');
+//
+//
+//var def = curAntigateBot.checkBalance();
 
-def.done(function(data){console.log(data);});
+//def.done(function(data){console.log(data);});
 /* Page event handler methods starts here */
-
-curPage.onUrlChanged = function(targetUrl) {
-    curPageURL = targetUrl; 
-};
-
-curPage.onLoadFinished = function(status) {
-    if (curPageURL == 'http://mail.yandex.ru/') {
-        onMainMailPageJump(status);
-    } else if (curPageURL.indexOf("https://passport.yandex.ru/") == 0) {
-        onMailRegisterPageJump(status);
-    }  else {
-        onUndefinedURLJump(status);
-    }
-};
 
 /* Page event handler methods ends here */
 
@@ -42,29 +27,7 @@ curPage.onLoadFinished = function(status) {
 
 /* Page jump handler functions starts here */
 
-// http://mail.yandex.ru/
-function onMainMailPageJump(status)
-{           
-    if (status == 'success') {
-        var resp = response.create('yandex', curPageURL, 'starting', status, 'unknown', 'Opening main mail page...');
-        console.log(JSON.stringify(resp));  
-        
-        var coords = curPage.evaluate(function() {
-            var evt = document.createEvent("MouseEvents");
-        
-            evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            document.getElementsByClassName("b-big-button")[0].dispatchEvent(evt);
-        });
-        
-        resp = response.create('yandex', curPageURL, 'processing', status, 'success', 'Click a link on the main mail page...');
-        console.log(JSON.stringify(resp));           
-    } else {     
-        resp = response.create('yandex', curPageURL, 'finishing', status, 'fail', 'Main mail page is not loading...');
-        console.log(JSON.stringify(resp)); 
-        phantom.exit();
-    }
-}
-
+/*
 // https://passport.yandex.ru/...
 function onMailRegisterPageJump(status)
 {
@@ -97,10 +60,10 @@ function onUndefinedURLJump(status)
     var resp = response.create('yandex', curPageURL, 'finishing', status, 'unknown', 'Undefined page...');
     console.log(JSON.stringify(resp));     
     phantom.exit();  
-}
+}*/
 
 /* Page jump handler functions ends here */
-
+/*
 curPage.settings.userAgent = 'WebKit/534.46 Mobile/9A405 Safari/7534.48.3';
-/*curPage.open('http://mail.yandex.ru/', function() {
+curPage.open('http://mail.yandex.ru/', function() {
 });*/
