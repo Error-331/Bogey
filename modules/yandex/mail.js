1f4778f0422ae5cb0240044afc2f22b8efda66ef
root <sergei-System-Product-Name>
2013-06-25 17:35:39 +0300

    add method to extract registration captcha

--- modules/yandex/mail.js
+++ modules/yandex/mail.js
@@ -22,18 +22,28 @@ var Mail = function(usrSystemKey)
     
     /* Private core methods starts here */
     
+    function logOut()
+    {
+        var curPage = this.getPage();
+        var def = deferred.create();
+        
+        def.resolve();
+        
+        return def;
+    }
+    
     function openMainPage()
     {
         var curPage = this.getPage();
         var def = deferred.create();
         
-        obj.logProcess(mainPageURL, 'starting', 'unknown', 'unknown', 'Opening main page...'); 
+        obj.logProcess(obj.getCurPageURL(), 'starting', 'unknown', 'unknown', 'Opening main page...'); 
         curPage.open(mainPageURL, function(status) {
             if (status == 'success') {
-                obj.logProcess(mainPageURL, 'finishing', status, 'success', 'Main page successfuly opened...'); 
+                obj.logProcess(obj.getCurPageURL(), 'finishing', status, 'success', 'Main page successfuly opened...'); 
                 def.resolve();
             } else {
-                obj.logProcess(mainPageURL, 'finishing', status, 'fail', 'Cannot open main page...'); 
+                obj.logProcess(obj.getCurPageURL(), 'finishing', status, 'fail', 'Cannot open main page...'); 
                 def.reject();
             }           
         });
@@ -43,48 +53,93 @@ var Mail = function(usrSystemKey)
     
     function openRegPage()
     {
+        var curPage = obj.getPage();
+        var def = deferred.create();
+                    
+        obj.logProcess(obj.getCurPageURL(), 'starting', 'unknown', 'unknown', 'Opening registration page...');          
+        obj.logOut().done(function() {
+            if (obj.getCurPageURL() != mainPageURL) {
+                obj.openMainPage().done(function(){
+
+                    curPage.evaluate(function() {
+                        var evt = document.createEvent("MouseEvents");      
+
+                        evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                        setTimeout(function(){
+                            document.getElementsByClassName("b-big-button")[0].dispatchEvent(evt);
+                        }, 3000);
+                    });
+
+                    obj.pushPageLoadFunc(function(status){
+                        obj.logProcess(obj.getCurPageURL(), 'processing', 'unknown', 'unknown', 'Click a "registration" link on main page...');  
+
+                        if (obj.getCurPageURL().indexOf("https://passport.yandex.ru/passport?mode=simplereg") == 0 && status == 'success') {
+                            obj.logProcess(obj.getCurPageURL(), 'finishing', status, 'success', 'Registration page successfuly opened...'); 
+                            def.resolve();
+                        }  else {
+                            obj.logProcess(obj.getCurPageURL(), 'finishing', status, 'fail', 'Cannot open registration page...'); 
+                            def.reject();
+                        }                    
+                    });
+                
+                }).fail(function(){
+                    obj.logProcess(obj.getCurPageURL(), 'finishing', 'fail', 'fail', 'Cannot open registration page...'); 
+                    def.reject();
+                });
+            }         
+        }).fail(function(){
+            obj.logProcess(obj.getCurPageURL(), 'finishing', 'fail', 'fail', 'Cannot open registration page...'); 
+            def.reject();
+        });
+         
+        return def;
+    }
+    
+    function extractRegPageCaptcha()
+    {    
         var curPage = this.getPage();
         var def = deferred.create();
         
-        obj.logProcess('', 'starting', 'unknown', 'unknown', 'Opening registration page...'); 
-        if (obj.getCurPageURL() != mainPageURL) {
-            obj.openMainPage().done(function(){
+        obj.logProcess(obj.getCurPageURL(), 'starting', 'unknown', 'unknown', 'Trying to extract captcha from registration page...');    
+        obj.openRegPage().done(function(){        
+            obj.logProcess(obj.getCurPageURL(), 'processing', 'success', 'unknown', 'Extracting captcha image position...');    
+            
+            var captchaCoords = curPage.evaluate(function (findOffset) {                               
+                var img = document.getElementById("captcha-img");  
+                if (img === null) {
+                    return false;
+                } else {
+                    return findOffset(img);
+                }
+            }, sandboxutils.findOffset);
 
-               curPage.evaluate(function() {
-                    var evt = document.createEvent("MouseEvents");      
-
-                    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-                    setTimeout(function(){
-                        document.getElementsByClassName("b-big-button")[0].dispatchEvent(evt);
-
-                    }, 4000);
-
-                });
-
-                obj.pushURLChangeFunc(function(){
-                    obj.logProcess(obj.getCurPageURL(), 'processing', 'unknown', 'unknown', 'Click a "registration" link on main page...');  
-                    
-                    if (obj.getCurPageURL().indexOf("https://passport.yandex.ru/passport?mode=simplereg") == 0) {
-                        obj.logProcess(obj.getCurPageURL(), 'finishing', 'success', 'success', 'Registration page successfuly opened...'); 
-                        def.resolve();
-                    }  else {
-                        obj.logProcess(obj.getCurPageURL(), 'finishing', 'fail', 'fail', 'Cannot open registration page...'); 
-                        def.reject();
-                    }                    
-                });
-                
-            }).fail(function(){
-                obj.logProcess('', 'finishing', 'fail', 'fail', 'Cannot open registration page...'); 
+            if (typeof captchaCoords != 'object') {             
+                obj.logProcess(obj.getCurPageURL(), 'finishing', 'success', 'fail', 'Cannot extract captcha image position...'); 
                 def.reject();
-            });
-        }
-          
-        return def;
+            } else {
+                curPage.clipRect = {top: captchaCoords.top, left: captchaCoords.left, width: captchaCoords.width, height: captchaCoords.height};
+                curPage.render("test.png");
+        
+                obj.logProcess(obj.getCurPageURL(), 'finishing', 'success', 'success', 'Captcha image successfully extracted...'); 
+                def.resolve();   
+            } 
+        }).fail(function(){
+            obj.logProcess(obj.getCurPageURL(), 'finishing', 'fail', 'fail', 'Cannot extract captcha from registration page...'); 
+            def.reject();
+        });
+        
+        
+        return def;    
     }
     
     /* Private core methods ends here */
     
     /* Privileged core methods starts here */
+    
+    this.logOut = function()
+    {
+        return this.startOp(logOut);
+    }    
     
     this.openMainPage = function()
     {
@@ -94,6 +149,11 @@ var Mail = function(usrSystemKey)
     this.openRegPage = function()
     {
         return this.startOp(openRegPage);
+    }
+    
+    this.extractRegPageCaptcha = function()
+    {
+        return this.startOp(extractRegPageCaptcha);
     }
     
     /* Privileged core methods ends here */
