aaa815a40922c7af0e7ba59086569613c5964c98
root <sergei-System-Product-Name>
2013-06-27 16:52:21 +0300

    add banch of new methods

--- modules/utils/fileutils.js
+++ modules/utils/fileutils.js
@@ -42,27 +42,103 @@ exports.checkImgName = function(imgName, defName)
     return imgName;
 }
 
-exports.isReadable = function(path)
-{
-    if (typeof path != 'string') {
-        throw 'Path is not a string';
-    }
-    
-    if (path.length <= 0) {
-        throw 'Path is zero length';
-    }
-    
-    return this.isReadable(path);
-}
-
 exports.extractPath = function(path)
 {
+    if (typeof path != 'string') {
+        throw 'Given path is not a string';
+    }    
     
+    if (path.length == 0) {
+        throw 'Given path is zero length';
+    }    
+    
+    var sep = path.lastIndexOf(fs.separator);
+     
+    if (sep == -1) {
+        try {
+            var ext = exports.extractExtension(path);
+            return '';
+        } catch(error) {
+            return path;
+        }  
+    } else if (sep == 0) {
+        try {
+            var ext = exports.extractExtension(path);
+            return fs.separator;
+        } catch(error) {
+            return path;
+        }          
+    } else {
+        return path.substr(0, sep + 1); 
+    }
 }
 
 exports.extractExtension = function(path)
+{    
+    if (typeof path != 'string') {
+        throw 'Given path is not a string';
+    }
+    
+    if (path.length == 0) {
+        throw 'Given path is zero length';
+    }
+    
+    var dot = path.lastIndexOf('.');
+    
+    if (dot == -1) {
+        throw 'Cannot extract file extension from given path';
+    }
+
+    return path.substr(dot + 1);
+}
+
+exports.addSeparator = function(path)
 {
     if (typeof path != 'string') {
-        
+        throw 'Given path is not a string';
     }
+    
+    if (path.length == 0) {
+        throw 'Given path is zero length';
+    }
+    
+    var sep = path.lastIndexOf(fs.separator);
+
+    if (sep == -1 || sep == 0) {
+        path = path + fs.separator;
+        return path;
+    } else {
+        if (sep < path.length) {
+            path = path + fs.separator;
+        } else {
+            return path;
+        }
+    }
+}
+
+exports.isPathReadable = function(path)
+{
+    var path = exports.extractPath(path); 
+    
+    if (fs.isDirectory(path) === true) {
+        return fs.isReadable(path);
+    } else {
+        throw 'Given path is not a directory';
+    }
+}
+
+exports.isPathWritable = function(path)
+{
+    var path = exports.extractPath(path); 
+    
+    if (fs.isDirectory(path) === true) {
+        return fs.isWritable(path);
+    } else {
+        throw 'Given path is not a directory';
+    }    
+} 
+
+exports.getDirSeparator = function()
+{
+    return fs.separator;
 }
