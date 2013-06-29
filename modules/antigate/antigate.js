5f5c053c906319931d53f52361161d09613c9438
Error-331 <red331@mail.ru>
2013-06-29 19:54:02 +0300

    add means to set parameters for captcha check

--- modules/antigate/antigate.js
+++ modules/antigate/antigate.js
@@ -15,14 +15,7 @@ var Antigate = function(configObj)
      */        
     
     var obj = this;
-    
-    /**
-     * @access private
-     * @var string API key
-     */        
-    
-    var systemKey = '';  
-    
+        
     /**
      * @access private
      * @var string URL to which common requests will be made
@@ -49,7 +42,7 @@ var Antigate = function(configObj)
      * @var integer timeout in milliseconds after which captcha upload process will be canceled
      */          
     
-    var uploadImageOpTimeout = 3000;
+    var uploadImageOpTimeout = 5000;
     
     /**
      * @access private
@@ -78,6 +71,13 @@ var Antigate = function(configObj)
      */     
     
     var checkCaptchaDelay = 7000;
+    
+    /**
+     * @access private
+     * @var string API key
+     */        
+    
+    var systemKey = '';      
     
     /**
      * @access private
@@ -144,7 +144,7 @@ var Antigate = function(configObj)
     
     /**
      * @access private
-     * @var integer indicates whether antigate service will send 'Access-Control-Allow-Origin: *' header in response on captcha upload
+     * @var integer indicates whether antigate service will send 'Access-Control-Allow-Origin: *' header in response to most requests
      */     
     
     var headerAcaoParam = 0;
@@ -366,16 +366,77 @@ var Antigate = function(configObj)
             if (status == 'success') {
                 obj.logProcess(obj.getCurPageURL(), 'processing', status, 'unknown', 'Succesfully open local form file...');
                 
-                // page preparation and submit
+                // set image to upload
                 curPage.uploadFile('input[name=file]', imagePath);
-                curPage.evaluate(function(key) {
+                
+                // page evalute
+                curPage.evaluate(function(systemKey, phraseParam, regsenseParam, numericParam, calcParam, minLenParam, maxLenParam, isRussianParam, maxBidParam, softIdParam, headerAcaoParam) {
                     var evt = document.createEvent("MouseEvents"); 
                     
-                    document.getElementsByName('key')[0].value = key;
+                    var inpts = document.getElementsByTagName('input');
+                    var form = document.getElementById('mainForm');
+                    
+                    var elm = null;
+                    var i = 0;
+                    
+                    // set values
+                    for (i = 0; i < inpts.length; i++) {
+                        switch(inpts[i].name) {
+                            case 'key':
+                                inpts[i].value = systemKey;
+                                break;
+                            case 'phrase':
+                                inpts[i].value = phraseParam;
+                                break;                          
+                            case 'regsense':
+                                inpts[i].value = regsenseParam;
+                                break;
+                            case 'numeric':
+                                inpts[i].value = numericParam;
+                                break;
+                            case 'calc':
+                                inpts[i].value = calcParam;
+                                break;
+                            case 'min_len':
+                                inpts[i].value = minLenParam;
+                                break;
+                            case 'max_len':
+                                inpts[i].value = maxLenParam;
+                                break;
+                            case 'is_russian':
+                                inpts[i].value = isRussianParam;
+                                break;   
+                            case 'header_acao':
+                                inpts[i].value = headerAcaoParam;
+                                break;                                  
+                        }
+
+                    }
+                    
+                    // create elements
+                    if (maxBidParam != 0) {
+                        elm = document.createElement("input");
+                        elm.name = 'max_bid';
+                        elm.type = 'hidden';
+                        elm.value = maxBidParam;
+                        
+                        form.appendChild(elm);
+                    }    
+                    
+                    if (softIdParam != '') {
+                        elm = document.createElement("input");
+                        elm.name = 'soft_id';
+                        elm.type = 'hidden';
+                        elm.value = softIdParam;
+                        
+                        form.appendChild(elm);
+                    }                      
+                                                            
+                    // send form
                     evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
                     document.getElementById('submitBtn').dispatchEvent(evt);                   
-                }, obj.getSystemKey());
-
+                }, systemKey, phraseParam, regsenseParam, numericParam, calcParam, minLenParam, maxLenParam, isRussianParam, maxBidParam, softIdParam, headerAcaoParam);
+                
                 // reject if timeout
                 setTimeout(function(){
                     if (!def.isDone()) {
@@ -439,12 +500,18 @@ var Antigate = function(configObj)
             return;
         }
         
-        
-        
-        
-        
         obj.setSystemKey(configObj.systemKey);
+        
         obj.setPhraseParam(configObj.phrase);
+        obj.setRegsenseParam(configObj.regsense);
+        obj.setNumericParam(configObj.numeric);
+        obj.setCalcParam(configObj.calc);
+        obj.setMinLenParam(configObj.minLen);
+        obj.setMaxLenParam(configObj.maxLen)
+        obj.setIsRussianParam(configObj.isRussian);
+        obj.setMaxBidParam(configObj.maxBid);
+        obj.setSoftIdParam(configObj.softId);
+        obj.setHeaderAcaoParam(configObj.headerAcao);         
     }  
 
     /**
@@ -529,8 +596,168 @@ var Antigate = function(configObj)
     this.getSystemKey = function()
     {
         return systemKey;
+    }   
+    
+    /**
+     * Method that returns 'phrase' parameter value.
+     *
+     * Simple method that returns 'phrase' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer indicates whether captcha consist of one word or multiple.
+     * 
+     */      
+    
+    this.getPhraseParam = function()
+    {
+        return phraseParam;
+    }   
+    
+    /**
+     * Method that returns 'regsense' parameter value.
+     *
+     * Simple method that returns 'regsense' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer indicates whether captcha is case sensitive or not
+     * 
+     */      
+    
+    this.getRegsenseParam = function()
+    {
+        return regsenseParam; 
+    }      
+    
+    /**
+     * Method that returns 'numeric' parameter value.
+     *
+     * Simple method that returns 'numeric' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer indicates whether captcha consist entirely of numbers or not
+     * 
+     */      
+    
+    this.getNumericParam = function()
+    {
+        return numericParam;
+    }      
+    
+    /**
+     * Method that returns 'calc' parameter value.
+     *
+     * Simple method that returns 'calc' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer indicates whether captcha presume mathematical operation or not
+     * 
+     */      
+    
+    this.getCalcParam = function()
+    {
+        return calcParam;
     }    
     
+    /**
+     * Method that returns 'min_len' parameter value.
+     *
+     * Simple method that returns 'min_len' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer minimum captcha text length
+     * 
+     */      
+    
+    this.getMinLenParam = function()
+    {
+        return minLenParam;
+    }     
+  
+    /**
+     * Method that returns 'max_len' parameter value.
+     *
+     * Simple method that returns 'max_len' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer maximum captcha text length
+     * 
+     */      
+    
+    this.getMaxLenParam = function()
+    {
+        return maxLenParam;
+    }        
+
+    /**
+     * Method that returns 'is_russian' parameter value.
+     *
+     * Simple method that returns 'is_russian' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer indicates whether captcha contains only russian letters or not
+     * 
+     */      
+    
+    this.getIsRussianParam = function()
+    {
+        return isRussianParam;
+    }      
+    
+    /**
+     * Method that returns 'max_bid' parameter value.
+     *
+     * Simple method that returns 'max_bid' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer bid value
+     * 
+     */      
+    
+    this.getMaxBidParam = function()
+    {
+        return maxBidParam; 
+    }   
+
+    /**
+     * Method that returns 'soft_id' parameter value.
+     *
+     * Simple method that returns 'soft_id' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return string software id
+     * 
+     */      
+    
+    this.getSoftIdParam = function()
+    {
+        return softIdParam; 
+    }  
+
+    /**
+     * Method that returns 'header_acao' parameter value.
+     *
+     * Simple method that returns 'header_acao' parameter value.
+     *
+     * @access privileged
+     * 
+     * @return integer indicates whether antigate service will send 'Access-Control-Allow-Origin: *' header in response to most requests
+     * 
+     */      
+    
+    this.getHeaderAcaoParam = function()
+    {
+        return headerAcaoParam;
+    } 
+  
     /* Privileged get methods ends here */
     
     /* Privileged set methods starts here */
@@ -556,7 +783,284 @@ var Antigate = function(configObj)
         
         systemKey = usrSystemKey;
     }
-      
+     
+    /**
+     * Method that sets 'phrase' parameter value.
+     *
+     * Simple method that sets 'phrase' parameter value. If 0 is passed - captcha consist of one word, if 1 is passed - captcha consist
+     * multiple words.
+     *
+     * @access privileged
+     * 
+     * @param integer phrase indicates whether captcha consist of one word or multiple.
+     * 
+     * @throws string
+     * 
+     */      
+    
+    this.setPhraseParam = function(phrase)
+    {
+        if (typeof phrase != 'number') {
+            throw '"phrase" parameter must be numeric';
+        }
+        
+        if (phrase > 0) {
+            phraseParam = 1;
+        } else {
+            phraseParam = 0;
+        }           
+    } 
+
+    /**
+     * Method that sets 'regsense' parameter value.
+     *
+     * Simple method that sets 'regsense' parameter value. If 0 is passed - captcha is case insensitive, if 1 is passed - captcha 
+     * is case sensitive.
+     *
+     * @access privileged
+     * 
+     * @param integer regsense indicates whether captcha is case sensitive or not.
+     * 
+     * @throws string
+     * 
+     */      
+    
+    this.setRegsenseParam = function(regsense)
+    {
+        if (typeof regsense != 'number') {
+            throw '"regsense" parameter must be numeric';
+        }
+        
+        if (regsense > 0) {
+            regsenseParam = 1;
+        } else {
+            regsenseParam = 0;
+        }           
+    }
+     
+    /**
+     * Method that sets 'numeric' parameter value.
+     *
+     * Simple method that sets 'numeric' parameter value. If 0 is passed - captcha may contain letters as well as numbers, 
+     * if 1 is passed - captcha contains only numbers, if 2 is passed - captcha contains only letters.
+     *
+     * @access privileged
+     * 
+     * @param integer numeric indicates whether captcha consist entirely of numbers or not
+     * 
+     * @throws string
+     * 
+     */      
+    
+    this.setNumericParam = function(numeric)
+    {
+        if (typeof numeric != 'number') {
+            throw '"numeric" parameter must be numeric';
+        }
+        
+        if (numeric <= 0) {
+            numericParam = 0;
+        } else if (numeric == 1) {
+            numericParam = 1;
+        } else {
+            numericParam = 2;
+        }           
+    }  
+    
+    /**
+     * Method that sets 'calc' parameter value.
+     *
+     * Simple method that sets 'calc' parameter value. If 0 is passed - captcha does not contain any numeric operations, 
+     * if 1 is passed - captcha contains a numeric operation.
+     *
+     * @access privileged
+     * 
+     * @param integer calc indicates whether captcha presume mathematical operation or not
+     * 
+     * @throws string
+     * 
+     */      
+    
+    this.setCalcParam = function(calc)
+    {
+        if (typeof calc != 'number') {
+            throw '"calc" parameter must be numeric';
+        }
+        
+        if (calc <= 0) {
+            calcParam = 0;
+        } else {
+            calcParam = 1;
+        }         
+    }  
+    
+    /**
+     * Method that sets 'min_len' parameter value.
+     *
+     * Simple method that sets 'min_len' parameter value. If 0 is passed - minimal length of the captcha may vary, if passed value 
+     * is greater then 0 and less than 20 - minimal length of the captcha must adhere to this value.
+     *
+     * @access privileged
+     * 
+     * @param integer minLen minimum captcha text length
+     * 
+     * @throws string
+     * 
+     */      
+  
+    this.setMinLenParam = function(minLen)
+    {
+        if (typeof minLen != 'number') {
+            throw '"min_len" parameter must be numeric';
+        }
+        
+        if (minLen <= 0) {
+            minLenParam = 0;
+        } else if (minLen > 0 && minLen <= 20){
+            minLenParam = minLen;
+        } else {
+            minLenParam = 20;
+        }         
+    }   
+    
+    /**
+     * Method that sets 'max_len' parameter value.
+     *
+     * Simple method that sets 'max_len' parameter value. If 0 is passed - maximal length of the captcha may vary, if passed value 
+     * is greater then 0 and less than 20 - maximal length of the captcha must adhere to this value.
+     *
+     * @access privileged
+     * 
+     * @param integer maxLen maximum captcha text length
+     * 
+     * @throws string
+     * 
+     */      
+  
+    this.setMaxLenParam = function(maxLen)
+    {
+        if (typeof maxLen != 'number') {
+            throw '"max_len" parameter must be numeric';
+        }
+        
+        if (maxLen <= 0) {
+            maxLenParam = 0;
+        } else if (maxLen > 0 && maxLen <= 20){
+            maxLenParam = maxLen;
+        } else {
+            maxLenParam = 20;
+        }         
+    }     
+       
+    /**
+     * Method that sets 'is_russian' parameter value.
+     *
+     * Simple method that sets 'is_russian' parameter value. If 0 is passed - no special actions is taken, if 1 is passed - will 
+     * be assumed that captcha contains russian letters.
+     *
+     * @access privileged
+     * 
+     * @param integer isRussian indicates whether captcha contains only russian letters or not
+     * 
+     * @throws string
+     * 
+     */      
+    
+    this.setIsRussianParam = function(isRussian)
+    {
+        if (typeof isRussian != 'number') {
+            throw '"is_russian" parameter must be numeric';
+        }
+        
+        if (isRussian <= 0) {
+            isRussianParam = 0;
+        } else {
+            isRussianParam = 1;
+        }         
+    }     
+    
+    /**
+     * Method that sets 'max_bid' parameter value.
+     *
+     * Simple method that sets 'max_bid' parameter value. Passed parameter must range from 0.001 to 0.1 in order to take effect. 
+     * Zero value can be passed to restrict use this parameter in requests.
+     *
+     * @access privileged
+     * 
+     * @param integer bid value    
+     * 
+     * @throws string
+     * 
+     */      
+    
+    this.setMaxBidParam = function(bid)
+    {
+        if (typeof bid != 'number') {
+            throw '"max_bid" parameter must be numeric';
+        }
+        
+        if (bid <= 0) {
+            maxBidParam = 0;
+        } else if (bid > 0 && bid < 0.001) {
+            maxBidParam = 0.001;
+        } else if (bid >= 0.001 && bid <= 0.1 ) {
+            maxBidParam = bid;
+        } else if (bid > 0.1) {
+            maxBidParam = 0.1;
+        }         
+    }   
+
+
+    /**
+     * Method that sets 'soft_id' parameter value.
+     *
+     * Simple method that sets 'soft_id' parameter value. Passing empty string to this parameter will prevent of using software id
+     * parameter in requests;
+     *
+     * @access privileged
+     * 
+     * @param string softId software id  
+     * 
+     * @throws string
+     * 
+     */      
+    
+    this.setSoftIdParam = function(softId)
+    {
+        if (typeof softId != 'string') {
+            throw '"soft_id" parameter must be string';
+        }
+        
+        softIdParam = softId;
+    } 
+    
+    /**
+     * Method that sets 'header_acao' parameter value.
+     *
+     * Simple method that sets 'header_acao' parameter value. If 0 is passed - no special headers will be send in response to the
+     * request to antigate service, if 1 is passed - 'Access-Control-Allow-Origin: *' will be send.
+     *
+     * @access privileged
+     * 
+     * @param integer headerAcao indicates whether antigate service will send 'Access-Control-Allow-Origin: *' header in response to most requests
+     * 
+     * @throws string
+     * 
+     */      
+    
+    this.setHeaderAcaoParam = function(headerAcao)
+    {
+        if (typeof headerAcao != 'number') {
+            throw '"header_acao" parameter must be numeric';
+        }
+        
+        if (headerAcao <= 0) {
+            headerAcaoParam = 0;
+        } else {
+            headerAcaoParam = 1;
+        }         
+    }    
+
     /* Privileged set methods ends here */  
 
     this.configureService(configObj);
