f39c4641ccea801d5e18699067543a5cb7d38be5
Error-331 <red331@mail.ru>
2013-07-20 00:26:55 +0300

    refactor code

--- scenes/register_yandex_email_proxy/index.js
+++ scenes/register_yandex_email_proxy/index.js
@@ -1,4 +1,2 @@
 // Modules include
-//var proxyScenario = require('../../modules/proxy/scenarios/check_proxy').create({});
-//proxyScenario.start();
-//console.log('all good!!');
+var proxyScenario = require('../../modules/proxy/scenarios/check_proxy');
