33aa906c8f9ef3132fd8a1d5a71238e3cb15745e
Error-331 <red331@mail.ru>
2013-08-02 23:28:00 +0300

    add documentation, add new method

--- modules/core/dummy.js
+++ modules/core/dummy.js
@@ -1,3 +1,41 @@
+/**
+ * Bogey
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU GENERAL PUBLIC LICENSE (Version 3)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to red331@mail.ru so we can send you a copy immediately.
+ *
+ * Module service is a part of PhantomJS framework - Bogey.
+ *
+ * @package Bogey
+ * @author Selihov Sergei Stanislavovich <red331@mail.ru>
+ * @copyright Copyright (c) 2013 Selihov Sergei Stanislavovich.
+ * @license http://www.gnu.org/licenses/gpl.html GNU GENERAL PUBLIC LICENSE (Version 3)
+ *
+ */
+
+/**
+ * Core modules of the framework.
+ *
+ * @subpackage core
+ * @author Selihov Sergei Stanislavovich <red331@mail.ru>
+ */
+
+/**
+ * Documents the Dummy class.
+ *
+ * Class for simulating user interaction with web page.
+ *
+ * @subpackage Dummy
+ * @author Selihov Sergei Stanislavovich <red331@mail.ru>
+ */
+
 // Modules include
 var deferred = require('../async/deferred');
 
@@ -23,6 +61,19 @@ var Dummy = function(usrPage)
     
     /* Private core methods starts here */
     
+    /**
+     * Method that checks user supplied parameters used in many methods.
+     *
+     * Supplied parameters must be object (array) with 'top' and 'left' numeric properties.
+     *
+     * @access private
+     *
+     * @param object coords object containing mouse coordinates
+     * 
+     * @throws string    
+     *
+     */       
+    
     function checkEssentials(coords)
     {
         if (page === null) {
@@ -46,14 +97,56 @@ var Dummy = function(usrPage)
     
     /* Privileged core methods starts here */
     
-    // coords.top coords.left
-    this.click = function(coords)
+    /**
+     * Method that simulates mouse click.
+     *
+     * Method sends to events to the page: 'mousemove' and 'click'.
+     *
+     * @access privileged
+     *
+     * @param object coords object containing mouse coordinates
+     * @param string button of the mouse
+     * 
+     * @throws string    
+     *
+     */
+    
+    this.click = function(coords, button)
     {
         checkEssentials(coords);
         
-        page.sendEvent('mousemove', coords.left, coords.top);
+        if (button != undefined) {
+            if (typeof button != 'string') {
+                throw 'Parameter "button" is not a string';
+            }
+            
+            button = button.toLowerCase();
+            
+            if (button != 'left' && button != 'right') {
+                throw 'Invalid "button" parameter: "' + button + '"';
+            }
+            
+        } else {
+            button = 'left';
+        }
+             
+        page.sendEvent('mousemove', coords.left, coords.top, button);
         page.sendEvent('click');
     }
+    
+    /**
+     * Method that simulates user entering input.
+     *
+     * Method uses click() method to actually click on the input field and set focus on it, after that actual text is entered.
+     *
+     * @access privileged
+     *
+     * @param object coords object containing mouse coordinates (coordinates of the input)
+     * @param string text which will be entered
+     * 
+     * @throws string    
+     *
+     */    
     
     this.fillTextInput = function(coords, text)
     {
@@ -66,9 +159,45 @@ var Dummy = function(usrPage)
         page.sendEvent('keypress', text, null, null);
     }
     
+    /**
+     * Method that simulates user sequentially entering text for multiple input fields.
+     *
+     * Method uses click() method to actually click on the input field and set focus on it, after that actual text is entered.
+     *
+     * @access privileged
+     *
+     * @param array bunch of objects containing coordinates for each input and text to be entered
+     * 
+     * @throws string    
+     *
+     */      
+    
+    this.fillTextInputBunch = function(bunch)
+    {
+        if (typeof bunch != 'object') {
+            throw 'Supplied "bunch" parameter is not an object';
+        }
+        
+        var key = null;
+        for (key in bunch) {
+            obj.fillTextInput(bunch[key], bunch[key].text);
+        }
+    }
+    
     /* Privileged core methods ends here */
     
     /* Privileged get methods starts here */
+    
+    /**
+     * Method that returns current page object.
+     *
+     * Simple method that returns current page object.
+     *
+     * @access privileged
+     * 
+     * @return object page.
+     * 
+     */       
     
     this.getPage = function()
     {
@@ -78,6 +207,19 @@ var Dummy = function(usrPage)
     /* Privileged get methods ends here */
     
     /* Privileged set methods starts here */
+    
+    /**
+     * Method that sets current page object.
+     *
+     * Simple method that sets current page object.
+     *
+     * @access privileged
+     * 
+     * @param object usrPage page object
+     * 
+     * @throws string 
+     * 
+     */     
     
     this.setPage = function(usrPage)
     {
