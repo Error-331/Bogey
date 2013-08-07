3ea4a158f3542a96ff2068ac6924bc38a2c27149
Error-331 <red331@mail.ru>
2013-08-08 01:16:12 +0300

    small changes

--- scenes/test/index.js
+++ scenes/test/index.js
@@ -1,3 +1,6 @@
-var od = require('../../modules/odnoklassniki/services/base').create();
+var od = require('../../modules/odnoklassniki/services/base').create({
+    'persistCookies': false,
+    'modulesPath': '/var/www/phantomjs_scripts/modules/'
+});
 
 od.logIn();
