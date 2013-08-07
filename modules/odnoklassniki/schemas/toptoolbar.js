40aad067f77f7d8d07f7dca72a67c6962763e3fd
Error-331 <red331@mail.ru>
2013-08-08 01:17:07 +0300

    add odnoklasniki top toolbar scheme

---
+++ modules/odnoklassniki/schemas/toptoolbar.js
@@ -0,0 +1,31 @@
+if (Bogey == undefined){
+    var Bogey = {};
+}
+
+if (Bogey.odnoklassniki == undefined) {
+    Bogey.odnoklassniki = {};
+}
+
+if (Bogey.odnoklassniki.schemas == undefined) {
+    Bogey.odnoklassniki.schemas = {};
+}
+
+Bogey.odnoklassniki.schemas.topToolbar = {
+    elm1: {
+        sel: '#hook_Block_Portal',
+                    
+        is_single: true,
+        sub_is_single: true,
+                    
+        sub: {
+            elm1: {
+                sel: '#portal-headline_login',
+                sub: {
+                    elm1: {
+                        sel: '.portal-headline__login__link'
+                    }
+                }
+            }
+        }
+    }
+};
