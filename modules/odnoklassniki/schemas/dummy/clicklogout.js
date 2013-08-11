c076332b6d1cf8f0c0804669b82d55e2d32060dc
Error-331 <red331@mail.ru>
2013-08-11 22:46:44 +0300

    add logut dummy schema

---
+++ modules/odnoklassniki/schemas/dummy/clicklogout.js
@@ -0,0 +1,13 @@
+exports.schema = {
+    step1: {
+        op: 'click',
+                
+        left: 0,
+        top: 0,
+        
+        offset_left: 0,
+        offset_top: 0,
+        
+        delay_before: 1000
+    }
+}
