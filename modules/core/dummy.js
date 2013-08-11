5233c14abff48b2b33edb92a7d17f49dc3d20024
Error-331 <red331@mail.ru>
2013-08-11 14:32:02 +0300

    compliterly rewrite dummy class

--- modules/core/dummy.js
+++ modules/core/dummy.js
@@ -62,56 +62,278 @@ var Dummy = function(usrService)
     /* Private core methods starts here */
     
     /**
-     * Method that checks user supplied parameters used in many methods.
+     * Method that checks coordinates of the current schema element.
      *
-     * Supplied parameters must be object (array) with 'top' and 'left' numeric properties.
+     * Supplied parameters must be object with 'top' and 'left' numeric properties.
      *
      * @access private
      *
-     * @param object coords object containing mouse coordinates
+     * @param object elm current schema element
      * 
      * @throws string    
      *
      */       
     
-    function checkEssentials(coords)
-    {
-        if (page === null) {
-            throw 'Page is not set';
-        }
-        
-        if (typeof coords != 'object') {
-            throw 'Coordinates object type mismatch';
-        }     
-        
-        if (coords.top == undefined) {
+    function checkCoords(elm)
+    {                
+        if (elm.top == undefined) {
             throw 'Coordinates top point is not set';
         }
         
-        if (coords.left == undefined) {
+        if (elm.left == undefined) {
             throw 'Coordinates left point is not set';
         }
     }
     
-    function checkSchemaElm(elm)
+    /**
+     * Method that checks "delay_before" parameter of the current schema element.
+     *
+     * Method checks if current parameter exist, makes a delay if necessary and executes user defined function.
+     *
+     * @access private
+     *
+     * @param object elm current schema element
+     * @param function func user defined function
+     * 
+     * @return object deferred object.    
+     *
+     */     
+    
+    function checkDelayBefore(elm, func)
     {
         var def = deferred.create();
+        
+        if (typeof func != 'function') {
+            def.reject('Parameter "func" must be function');
+            return def;
+        }
+        
+        if (elm.delay_before != undefined) {
+            if (typeof elm.delay_before != 'number' || elm.delay_before <= 0) {
+                def.reject('Invalid "delay_before" parameter: "' + elm.delay_before + '"');
+                return def;
+            }
+            
+            setTimeout(function(){
+                func();
+                def.resolve();
+            }, elm.delay_before);                       
+        } else {
+            func();
+            def.resolve();   
+        }
+               
+        return def;
+    }
+    
+    /**
+     * Method that checks "delay_after" parameter of the current schema element.
+     *
+     * Method checks if current parameter exist and makes a delay if necessary.
+     *
+     * @access private
+     *
+     * @param object elm current schema element
+     * 
+     * @return object deferred object.    
+     *
+     */      
+    
+    function checkDelayAfter(elm)
+    {
+        var def = deferred.create();
+        
+        if (elm.delay_after != undefined) {
+            if (typeof elm.delay_after != 'number' || elm.delay_after <= 0) {
+                def.reject('Invalid "delay_after" parameter: "' + elm.delay_after + '"');
+                return def;
+            }
+            
+            setTimeout(function(){
+                def.resolve();
+            }, elm.delay_after);                       
+        } else {
+            def.resolve();   
+        }
+               
+        return def;        
+    }
+    
+    /**
+     * Method that simulates mouse click.
+     *
+     * Method sends to events to the page: 'mousemove' and 'click'.
+     *
+     * @access private
+     *
+     * @param object elm current element of the schema
+     * 
+     * @return object deferred object.  
+     *
+     */    
+    
+    function click(elm)
+    {
+        var def = deferred.create();
+        var page = service.getPage();  
+        
+        var execFunc = function(){
+            page.sendEvent('mousemove', elm.left, elm.top, elm.btn);
+            page.sendEvent('click');       
+        }
+        
+        // check coords
+        try{
+            checkCoords(elm);
+        } catch(e) {
+            def.reject(e);
+            return def;
+        }    
+        
+        // check offset
+        if (elm.offset != undefined) {
+            if (typeof elm.offset != 'number') {
+                def.reject('Offset must be numeric');
+                return def;
+            }
+            
+            elm.top += elm.offset;
+            elm.left += elm.offset;
+        }
+        
+        // check mouse button
+        if (elm.btn != undefined) {
+            if (typeof elm.btn != 'string') {
+                def.reject('"btn" parameter must be string');
+                return def;
+            }
+            
+            elm.btn = elm.btn.toLowerCase();
+        } else {
+            elm.btn = 'left';
+        }
+        
+        // check delay_before
+        checkDelayBefore(elm, execFunc).done(function(){
+            // check delay_after
+            checkDelayAfter(elm).done(function(){
+                def.resolve();
+            }).fail(function(error){
+                def.reject(error);
+                return def;
+            });
+        }).fail(function(error){
+            def.reject(error);
+            return def;            
+        });
+                
+        return def;
+    }
+    
+    /**
+     * Method that simulates user entering input.
+     *
+     * Method actually click on the input field and set focus on it, after that actual text is entered.
+     *
+     * @access private
+     *
+     * @param object elm current element of the schema
+     * 
+     * @return object deferred object.  
+     *
+     */        
+    
+    function fillTextInput(elm)
+    {
+        var def = deferred.create();
+        var page = service.getPage();  
+        
+        var execFunc = function(){
+            page.sendEvent('mousemove', elm.left, elm.top, 'left');
+            page.sendEvent('click');   
+                
+            page.sendEvent('keypress', elm.text, null, null);  
+        }        
+        
+        // check coords
+        try{
+            checkCoords(elm);
+        } catch(e) {
+            def.reject(e);
+            return def;
+        }  
+        
+        // check text
+        if (typeof elm.text != 'string') {
+            def.reject('Invalid parameter "text"');
+            return def;
+        }
+
+        // check delay_before
+        checkDelayBefore(elm, execFunc).done(function(){
+            // check delay_after
+            checkDelayAfter(elm).done(function(){
+                def.resolve();
+            }).fail(function(error){
+                def.reject(error);
+                return def;
+            });
+        }).fail(function(error){
+            def.reject(error);
+            return def;            
+        });
+        
+        return def;
+    }
+    
+    /**
+     * Method that processes "dummy" schema element.
+     *
+     * Method processes "dummy" schema element, based on the data of the element current method executes corresponding action.
+     *
+     * @access private
+     *
+     * @param object elm "dummy" schema element
+     * 
+     * @return object deferred object.
+     *
+     */       
+        
+    function runSchemaElm(elm)
+    {
+        var def = deferred.create();
+        var subDef = null;
         
         if (typeof elm != 'object') {
             def.reject('Schema element is not an object');
         }
         
-        if (typeof elm['op'] == 'string') {
+        if (typeof elm.op != 'string') {
             def.reject('Schema operation cannot be undefined');
         }
         
-        def.resolve();
-        return def;
-    }
-    
-    function runSchemaElm(elm)
-    {
-        var def = deferred.create();
+        elm.op = elm.op.toLowerCase();
+        
+        switch(elm.op){
+            case 'click':
+                subDef = click(elm);
+                break;
+                
+            case 'filltextinput':
+                subDef = fillTextInput(elm);
+                break;
+            
+            default:
+                def.reject('Unrecognised operation: "' + elm.op + '"');
+                return def;
+                break;
+        }
+
+        subDef.done(function(){
+            def.resolve();
+        }).fail(function(error){
+            def.reject(error);
+        });
         
         return def;
     }
@@ -119,98 +341,22 @@ var Dummy = function(usrService)
     /* Private core methods ends here */    
     
     /* Privileged core methods starts here */
-        
+    
     /**
-     * Method that simulates mouse click.
+     * Method that runs "dummy" schema.
      *
-     * Method sends to events to the page: 'mousemove' and 'click'.
+     * Method runs user defined "dummy" schema which is used to simulate real user interaction with web page.
      *
      * @access privileged
      *
-     * @param object coords object containing mouse coordinates
-     * @param string button of the mouse
+     * @param object schema "dummy" schema
      * 
-     * @throws string    
-     *
-     */
-    
-    this.click = function(coords, button)
-    {
-        checkEssentials(coords);
-        var def = deferred.create();
-        
-        if (button != undefined) {
-            if (typeof button != 'string') {
-                throw 'Parameter "button" is not a string';
-            }
-            
-            button = button.toLowerCase();
-            
-            if (button != 'left' && button != 'right') {
-                throw 'Invalid "button" parameter: "' + button + '"';
-            }
-            
-        } else {
-            button = 'left';
-        }
-             
-        var page = service.getPage();     
-             
-        page.sendEvent('mousemove', coords.left, coords.top, button);
-        page.sendEvent('click');     
-    }
-    
-    /**
-     * Method that simulates user entering input.
-     *
-     * Method uses click() method to actually click on the input field and set focus on it, after that actual text is entered.
-     *
-     * @access privileged
-     *
-     * @param object coords object containing mouse coordinates (coordinates of the input)
-     * @param string text which will be entered
+     * @return object deferred object.
      * 
-     * @throws string    
-     *
-     */    
-    
-    this.fillTextInput = function(coords, text)
-    {
-        obj.click(coords);
-        
-        if (typeof text != 'string') {
-            throw 'Supplied text value is not a string';
-        }
-        
-        var page = service.getPage();  
-        page.sendEvent('keypress', text, null, null);
-    }
-    
-    /**
-     * Method that simulates user sequentially entering text for multiple input fields.
-     *
-     * Method uses click() method to actually click on the input field and set focus on it, after that actual text is entered.
-     *
-     * @access privileged
-     *
-     * @param array bunch of objects containing coordinates for each input and text to be entered
-     * 
-     * @throws string    
+     * @throws string   
      *
      */      
-    
-    this.fillTextInputBunch = function(bunch)
-    {
-        if (typeof bunch != 'object') {
-            throw 'Supplied "bunch" parameter is not an object';
-        }
-        
-        var key = null;
-        for (key in bunch) {
-            obj.fillTextInput(bunch[key], bunch[key].text);
-        }
-    }
-    
+                   
     this.runSchema = function(schema)
     {
         if (typeof schema != 'object') {
@@ -230,50 +376,32 @@ var Dummy = function(usrService)
             keys.push(key);
         }
         
-        if (keys.length == 0) {
-            def.resolve();
-            return def;
+        var doneFunc = function(){
+            page.viewportSize = service.popViewportSize(); 
         }
-        
-        var loopFunc = function() {
+                
+        var loopFunc = function() {          
+            if (keys[i] == undefined) {
+                def.resolve();
+                return;
+            }
             
-            
-            runSchemaElm().done(function(){
+            runSchemaElm(schema[keys[i]]).done(function(){     
+                i++;
                 loopFunc();
-            });       
+            }).fail(function(error){
+                def.reject(error);
+                return;
+            });                    
         }
         
         // prepare viewport
         service.pushViewportSize(page.viewportSize); 
         page.viewportSize = {width: 800, height: 600};
-                
-                
-        
-             
-        checkSchemaElm(schema[keys[i]]).done(function(){
-            
-        }).fail(function(error){
-            def.reject(error);
-        });
-        
-        /*if (i == 0) {
-            if (keys[i] != undefined) {
-                switch schema[]
-            }   
-        } */       
-                     
-                
-            
-
-        
-
-        phantom.exit();    
-                
-                
-        // restore viewport
-        page.viewportSize = service.popViewportSize(); 
-        
-        
+                       
+        loopFunc();
+       
+        def.done(doneFunc).fail(doneFunc);   
         return def;
     }
     
@@ -288,7 +416,7 @@ var Dummy = function(usrService)
      *
      * @access privileged
      * 
-     * @return object page.
+     * @return object service.
      * 
      */       
     
