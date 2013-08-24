71f5b8c86c3678e484a99db5b79c5d8c5a2825c9
Error-331 <red331@mail.ru>
2013-08-24 19:13:57 +0300

    make scheme be able to include into dummy schemas

--- modules/yandex/schemas/sandbox/mail/validation/regform.js
+++ modules/yandex/schemas/sandbox/mail/validation/regform.js
@@ -13,57 +13,62 @@ if (Bogey.yandex.schemas == undefined) {
 Bogey.yandex.schemas.regForm = {
     elm1: {
         sel: 'input#firstname',
-        func: Bogey.utils.findOffset,
-        varName: '$firstName'
+        func: 'Bogey.utils.findOffset',
+        varName: 'firstName'
     },
     elm2: {
         sel: 'input#lastname',
-        func: Bogey.utils.findOffset,
-        varName: '$lastName'
+        func: 'Bogey.utils.findOffset',
+        varName: 'lastName'
     },
     elm3: {
         sel: 'input#login',
-        func: Bogey.utils.findOffset,
-        varName: '$login'
+        func: 'Bogey.utils.findOffset',
+        varName: 'login'
     },
     elm4: {
         sel: 'input#password',
-        func: Bogey.utils.findOffset,
-        varName: '$password'
+        func: 'Bogey.utils.findOffset',
+        varName: 'password'
     },
     elm5: {
         sel: 'input#password_confirm',
-        func: Bogey.utils.findOffset,
-        varName: '$passwordConfirm'
+        func: 'Bogey.utils.findOffset',
+        varName: 'passwordConfirm'
     },    
     elm6: {
         sel: 'select#hint_question_id',
-        func: Bogey.utils.findOffset,
-        varName: '$hintQuestionId'
+        func: 'Bogey.utils.findOffset',
+        varName: 'hintQuestionId'
     },   
     elm7: {
         sel: 'input#hint_answer',
-        func: Bogey.utils.findOffset,
-        varName: '$hintAnswer'
+        func: 'Bogey.utils.findOffset',
+        varName: 'hintAnswer'
     },          
     elm8: {
         sel: 'input#phone_number',
-        func: Bogey.utils.findOffset,
-        varName: '$phoneNumber'
+        func: 'Bogey.utils.findOffset',
+        varName: 'phoneNumber'
     },   
     elm9: {
         sel: 'img#captcha_img',
-        func: Bogey.utils.findOffset,
-        varName: '$captchaImg'
+        func: 'Bogey.utils.findOffset',
+        varName: 'captchaImg'
     },   
     elm10: {
         sel: 'input#answer',
-        func: Bogey.utils.findOffset,
-        varName: '$answer'
+        func: 'Bogey.utils.findOffset',
+        varName: 'answer'
     },
     elm11: {
         sel: 'button[type="submit"]',
-        func: Bogey.utils.findOffset,
-        varName: '$submitBtn'
+        func: 'Bogey.utils.findOffset',
+        varName: 'submitBtn'
     }     
 };
+
+// export for dummy schema
+if (typeof exports != 'undefined') {
+    exports.schema = Bogey.yandex.schemas.regForm;
+} 
