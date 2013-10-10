79a6a44b796ccee64d8233aa6f6fb5013301d52d
Error-331 <red331@mail.ru>
2013-10-10 23:51:49 +0300

    fix errors

--- modules/yandex/schemas/sandbox/mail/validation/invalidregformdata.js
+++ modules/yandex/schemas/sandbox/mail/validation/invalidregformdata.js
@@ -12,51 +12,81 @@ if (Bogey.yandex.schemas == undefined) {
 
 Bogey.yandex.schemas.invalidRegFormData = {
     params: {
-        invert: true
+        invert: false
     },
     
     elm1: {
-        sel: 'div.b-svarx__err_id_firstname_missingvalue',
+        sel: 'div.b-svarx__err_id_firstname_missingvalue.b-svarx__err_visible_no',
         errorMes: 'First name is not set'
     },
 
     elm2: {
-        sel: 'div.b-svarx__err_id_lastname_missingvalue',
+        sel: 'div.b-svarx__err_id_lastname_missingvalue.b-svarx__err_visible_no',
         errorMes: 'Last name is not set'
     },    
-    
+
     elm3: {
-        sel: 'div.b-svarx__err_id_login_notavailable',
+        sel: 'div.b-svarx__err_id_login_missingvalue.b-svarx__err_visible_no',
         errorMes: 'Login is not set'
-    },    
+    },      
     
     elm4: {
-        sel: 'div.b-svarx__err_id_password_tooshort',
-        errorMes: 'Password is not set'
-    },       
+        sel: 'div.b-svarx__err_id_login_notavailable.b-svarx__err_visible_no',
+        errorMes: 'Login is not available'
+    },    
     
     elm5: {
-        sel: 'div.b-svarx__err_id_password_confirm_notequal',
-        errorMes: 'Password confirm is not set'
-    },     
-
+        sel: 'div.b-svarx__err_id_password_missingvalue.b-svarx__err_visible_no',
+        errorMes: 'Password is not set'        
+    },
+    
     elm6: {
-        sel: 'div.b-svarx__err_id_hint_question_id_missingvalue',
-        errorMes: 'Hint question is not set'
+        sel: 'div.b-svarx__err_id_password_tooshort.b-svarx__err_visible_no',
+        errorMes: 'Password too short'
     }, 
     
     elm7: {
-        sel: 'div.b-svarx__err_id_hint_answer_missingvalue',
+        sel: 'div.b-svarx__err_id_password_prohibitedsymbols.b-svarx__err_visible_no',
+        errorMes: 'Password symbols prohibited'
+    },    
+    
+    elm8: {
+        sel: 'div.b-svarx__err_id_password_toolong.b-svarx__err_visible_no',
+        errorMes: 'Password too long'
+    },     
+
+    elm9: {
+        sel: 'div.b-svarx__err_id_password_weak.b-svarx__err_visible_no',
+        errorMes: 'Password too week'
+    },
+     
+    elm10: {
+        sel: 'div.b-svarx__err_id_password_confirm_missingvalue.b-svarx__err_visible_no',
+        errorMes: 'Password confirm is not set'
+    },  
+ 
+    elm11: {
+        sel: 'div.b-svarx__err_id_password_confirm_notequal.b-svarx__err_visible_no',
+        errorMes: 'Password confirm is not equal'
+    },     
+
+    elm12: {
+        sel: 'div.b-svarx__err_id_hint_question_id_missingvalue.b-svarx__err_visible_no',
+        errorMes: 'Hint question is not set'
+    }, 
+    
+    elm13: {
+        sel: 'div.b-svarx__err_id_hint_answer_missingvalue.b-svarx__err_visible_no',
         errorMes: 'Hint answer is not set'
     },  
     
-    elm8: {
-        sel: 'div.b-svarx__err_id_phone_number_badphonenumber',
+    elm14: {
+        sel: 'div.b-svarx__err_id_phone_number_badphonenumber.b-svarx__err_visible_no',
         errorMes: 'Phone number is not set or incorrect'
     },   
     
-    elm9: {
-        sel: 'div.b-svarx__err_id_answer_missingvalue',
+    elm15: {
+        sel: 'div.b-svarx__err_id_answer_missingvalue.b-svarx__err_visible_no',
         errorMes: 'Captcha is not set or incorrect'
     }    
 };
