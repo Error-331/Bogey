c0079baa0cdfc9ec6f6c399630415744d056d8a8
Error-331 <sd34cxz32d>
2014-03-05 01:39:28 +0200

    Add scene for refresh box token.

---
+++ scenes/refresh_box_token/index.js
@@ -0,0 +1,5 @@
+var newScenario = require('../../modules/advisory_board/scenarios/refresh_box_token').create().start();
+
+
+
+//sudo phantomjs --cookies-file=testcook.txt index.js viewportWidth=1024 viewportHeight=2000 debugSandbox=true modulesPath=/var/www/phantomjs_scripts/modules/ reloginOnStart=true firstName=ZopaTestFirst lastName=ZopaTestLast login=zopa password=sd334gfa231f passwordConfirm=sd334gfa231f optionIndex=10 hintAnswer=bbb systemKey=bfd936a2bd18bb817df7e95d3621a7c8 phrase=0 regsense=0 numeric=0 calc=0 minLen=0 maxLen=0 isRussian=0 ping=0
