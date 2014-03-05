f905707f3b18c6253e954e096128ba4ce611ea35
sselihov <sselihov@lohila.com>
2014-03-05 16:14:44 +0200

    minor fixes

--- scenes/refresh_box_token/index.js
+++ scenes/refresh_box_token/index.js
@@ -2,4 +2,4 @@ var newScenario = require('../../modules/advisory_board/scenarios/refresh_box_to
 
 
 
-//sudo phantomjs --cookies-file=testcook.txt index.js clientId= clientSecret= clientEmail= clientPassword= debugSandbox=true modulesPath=/var/www/Bogey/modules/ reloginOnStart=true
+//sudo phantomjs --cookies-file=testcook.txt index.js clientBoxParamSk= clientId= clientSecret= clientEmail= clientPassword= debugSandbox=true modulesPath=/var/www/Bogey/modules/ reloginOnStart=true
