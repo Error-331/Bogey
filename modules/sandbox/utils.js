9ea96a27b1f92deeb9eb97afd516a0773edc9aed
Error-331 <sd34cxz32d>
2014-06-23 18:55:34 +0300

    Rewrite findOffset() method.

--- modules/sandbox/utils.js
+++ modules/sandbox/utils.js
@@ -8,38 +8,8 @@ if (Bogey.utils == undefined){
 
 Bogey.utils.findOffset = function(obj) 
 {
-    var obj2 = obj;
-    
-    var curtop = 0;
-    var curleft = 0;
-    
-    var curWidth = obj.offsetWidth;
-    var curHeight = obj.offsetHeight;
-     
-    if (document.getElementById || document.all) {
-        do  {
-            curleft += obj.offsetLeft - obj.scrollLeft;
-            curtop += obj.offsetTop - obj.scrollTop;
-                        
-            obj = obj.offsetParent;
-            obj2 = obj2.parentNode;
-              
-            if (!obj) {
-                break;
-            }  
-              
-            while (obj2 != obj) {
-                curleft -= obj2.scrollLeft;
-                curtop -= obj2.scrollTop;
-                obj2 = obj2.parentNode;
-            }
-        } while (obj.offsetParent)
-    } else if (document.layers) {
-        curtop += obj.y;
-            curleft += obj.x;
-    }
-                
-    return {top: curtop, left: curleft, width: curWidth, height: curHeight};
+    var edges = obj.getBoundingClientRect();
+    return {top: edges.top, left: edges.left, width: edges.width, height: edges.height};
 }
 
 Bogey.utils.findChildrenOffset = function(parElm, sel)
