947ed975b7be33ef2077edd97f0215e681d195bd
Error-331 <sd34cxz32d>
2014-06-24 01:42:44 +0300

    Add scenario for fetching group users.

---
+++ modules/odnoklassniki/scenarios/fetchgroupusers.js
@@ -0,0 +1,75 @@
+// Modules include
+var scenario = require('../../core/scenario');
+var deferred = require('../../async/deferred');
+
+var odnoklassniki = require('../services/base');
+
+var fileUtils = require('../../utils/fileutils');
+
+var FetchGroupUsers = function()
+{
+    scenario.constFunc.call(this, 'fetch_group_users');
+
+    /* Private members starts here */
+
+    /**
+     * @access private
+     * @var object link to the current object
+     */
+
+    var obj = this;
+
+    /**
+     * @access private
+     * @var object instance of the Odnoklassniki service
+     */
+
+    var curBase;
+
+    /* Private members ends here */
+
+    /* Privileged core methods starts here */
+
+    this.start = function()
+    {
+        var options = obj.getOptions();
+
+        options.scenario = obj;
+        options.logsnapdir = 'snapshots' + fileUtils.getDirSeparator() + Date.now();
+
+        curBase = odnoklassniki.create(options);
+
+        curBase.openGroupPage('pro.jenshin.mujchin').done(function(){
+
+        }).fail(function(){
+
+        });
+
+
+    }
+
+    /**
+     * Method that stops current scenario.
+     *
+     * Method sends response based on the information stored in the current object and calls phantom.exit().
+     *
+     * @access privileged
+     *
+     */
+
+    this.stop = function()
+    {
+        phantom.exit();
+    }
+
+    /* Privileged core methods ends here */
+}
+
+exports.create = function create() {
+    "use strict";
+
+    FetchGroupUsers.prototype = scenario.create('fetch_group_users');
+    FetchGroupUsers.prototype.constructor = FetchGroupUsers;
+
+    return new FetchGroupUsers('fetch_group_users');
+};
